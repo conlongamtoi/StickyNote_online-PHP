4b0aad32f13bacb9592670893b8e0313f096cb37
conlongamtoi81 <conlongamtoi81@gmail.com>
2021-04-07 11:34:24 +0700

    Add files via upload

--- result.php
+++ result.php
@@ -9,14 +9,34 @@
 		$uname=$_POST["uname"];
 		$pwd=$_POST["pwd"];
 		$pwd1=$_POST["pwd1"];
-		if ($pwd==$pwd1) {
-		/* Kết nối máy chủ MySQL. Máy chủ có cài đặt mặc định (user là 'root' và không có mật khẩu) */
+		//kiểm tra trùng lặp
+		$match=false;
+		// tạo connection
+		$link = mysqli_connect("localhost","root","","info");
+		// kiểm tra connection
+		if (!$link) {
+		    die("Connection failed: " . mysqli_connect_error());
+		}
+		 
+		$sql1 = "SELECT id, Username, FROM login";
+		$result = mysqli_query($link, $sql1);
+		 
+		if (mysqli_num_rows($result) > 0) {
+		    // kiểm tra tên và mật khẩu
+		    while($row = mysqli_fetch_assoc($result)) {
+		    	if ($uname==$row["Username"]){
+		    		$match=true;
+		    	}
+		    }
+		}
+		 if ($pwd==$pwd1 and $pwd!='' and $uname!='') {
+		/* Kết nối máy chủ MySQL */
 		$link = mysqli_connect("localhost", "root", "", "info");
  
 		// Kiểm tra kết nối
 		if($link === false){
 		    die("ERROR: Không thể kết nối. " . mysqli_connect_error());
-		}
+		}	
 		 
 	// Thực thi câu lệnh insert
 		$sql = "INSERT INTO login (Username, Password) VALUES ('$uname', '$pwd')";
@@ -29,10 +49,10 @@
 	// Đóng kết nối
 		mysqli_close($link);
 		}else{
-			echo"Mật khẩu không khớp, vui lòng bấm Back và gõ lại";
+			echo "Lỗi mật khẩu, vui lòng quay lại";
 		}
 	?>
 	<button onclick="window.location.href='Trang_chu.php'">Về trang chủ</button>
-	</center>
+	</center> 
 </body>
 </html>
